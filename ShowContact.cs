b3c8d7afaa886bae54aa078881e7ce26d4742ef0
poojakashyap-2920 <pooja337026@gmail.com>
2024-02-06 16:11:06 +0530

    show file is added

---
+++ ShowContact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class ShowContact
+    {
+        public void Show()
+        {
+            foreach (Contact contact in Program.contacts)
+            {
+                Console.WriteLine($"FirstName : {contact.FirstName}\nLastName : {contact.LastName}\nAddress : {contact.Address}\nCity : {contact.City}\n State : {contact.State}\n Zip : {contact.Zip}\n Phno : {contact.Phno}\n Email : {contact.Email}");
+            }
+        }
+    }
+}
