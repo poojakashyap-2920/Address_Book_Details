4c4c01d4bc1b951efbe249b8d8afc3c3d3b92090
poojakashyap-2920 <pooja337026@gmail.com>
2024-02-06 15:47:59 +0530

    add file is added

---
+++ AddContact.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class AddContact
+    {
+        public void Add()
+        {
+            Console.WriteLine("**********  Contact detail....  ***********");
+            Console.WriteLine("Enter first name...");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter last name...");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter address...");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("Enter city...");
+            string city = Console.ReadLine();
+
+            Console.WriteLine("Enter state...");
+            string state = Console.ReadLine();
+
+            Console.WriteLine("Enter zip...");
+            int zip = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter phone number...");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("Enter email...");
+            string email = Console.ReadLine();
+            Program.Main(new string[] { });
+            Console.WriteLine("record added successfully...");
+            Console.ReadLine();
+
+        }
+    }
+
+}
