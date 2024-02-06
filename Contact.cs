4223953ca7aa5a54be063b094938b9f10e3a9269
poojakashyap-2920 <pooja337026@gmail.com>
2024-02-06 16:17:47 +0530

    contact class ia added

---
+++ Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Contact
+
+    {
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string Address { get; set; }
+        public virtual string City { get; set; }
+        public virtual string State { get; set; }
+        public virtual int Zip { get; set; }
+        public virtual long Phno { get; set; }
+        public virtual string Email { get; set; }
+    }
+}
