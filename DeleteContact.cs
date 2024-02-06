75510f603638be4fd254cb8850c83abdf8d4fbcc
poojakashyap-2920 <pooja337026@gmail.com>
2024-02-06 16:02:05 +0530

    DeleteContact is added

---
+++ DeleteContact.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class DeleteContact
+    {
+        public void Delete()
+        {
+
+            Console.WriteLine("Enter first name...");
+            string fn = Console.ReadLine();
+
+            Console.WriteLine("Enter last name...");
+            string ln = Console.ReadLine();
+
+
+            Contact contactToDelete = Program.contacts.FirstOrDefault(c => c.FirstName.Equals(fn) && c.LastName.Equals(ln));
+
+            if (contactToDelete != null)
+            {
+                Program.contacts.Remove(contactToDelete);
+                Console.WriteLine("Contact deleted successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Contact not found");
+            }
+            Console.WriteLine("contact deleted succesfully..");
+        }
+    }
+}
